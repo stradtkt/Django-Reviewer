32b6c9a9c8d5c89d645f87c473c6539e5b84a798
Kevin <stradtkt22@gmail.com>
2018-07-12 14:58:38 -0400

    Added models

--- apps/book_reviewers/models.py
+++ apps/book_reviewers/models.py
@@ -4,4 +4,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Review(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField(max_length=1000)
+    user = models.ForeignKey(User)
+    book = models.ForeignKey(Book)
